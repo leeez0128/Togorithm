80f54fde0347325101db1059ec32b441c8fa97f3
wndnjs9878 <leeez0128@gmail.com>
2021-11-21 21:45:07 +0900

    [leeez0128] 문자열 update 2941.py

--- leeez0128/baekjoon/문자열/2941.py
+++ leeez0128/baekjoon/문자열/2941.py
@@ -1,16 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+# 이렇게 풀린다는게 좀 이상..한데?...
+# def solution(word):
+#     cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+#     result = 0
+#     for atia in cro:
+#         result += word.count(atia)
+    
+#     print(len(word)-result)
 
+# 모범답안
 def solution(word):
     cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-    result = 0
     for atia in cro:
-        result += word.count(atia)
-    
-    print(len(word)-result)
-            
-
+        word = word.replace(atia, "*")
+    print(len(word))
 
 
 if __name__ == '__main__':
