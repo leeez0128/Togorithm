b55bc02f9143a9b65adc35628b7667ef658f5826
1998YUKI0331 <dkenl0110@dongguk.edu>
2021-11-29 22:05:09 +0900

    [1998YUKI0331] 백트래킹 14888.py

--- 1998YUKI0331/baekjoon/BackTracking/14888.py
+++ 1998YUKI0331/baekjoon/BackTracking/14888.py
@@ -3,32 +3,25 @@ import itertools
 input = sys.stdin.readline
 
 
-def calc(X, opNum, Y):
-    if opNum == 0:
-        return X + Y
-    elif opNum == 1:
-        return X - Y
-    elif opNum == 2:
-        return X * Y
-    elif opNum == 3:
-        if X < 0 and Y > 0:
-            return -1 * ((-1 * X) // Y)
-        else:
-            return X // Y
-
-
 def solution(N, Numbers, opNums):
     Max = float('-inf')
     Min = float('inf')
 
     Ops = sum([[x] * opNums[x] for x in range(len(opNums))], [])
     for i in itertools.permutations(Ops, len(Ops)):
-        temp = 0
+        temp = Numbers[0]
         for opIdx in range(len(i)):
-            if opIdx == 0:
-                temp = calc(Numbers[0], i[opIdx], Numbers[1])
-            else:
-                temp = calc(temp, i[opIdx], Numbers[opIdx + 1])
+            if i[opIdx] == 0:
+                temp += Numbers[opIdx + 1]
+            elif i[opIdx] == 1:
+                temp -= Numbers[opIdx + 1]
+            elif i[opIdx] == 2:
+                temp *= Numbers[opIdx + 1]
+            elif i[opIdx] == 3:
+                if temp < 0 and Numbers[opIdx + 1] > 0:
+                    temp = -1 * ((-1 * temp) // Numbers[opIdx + 1])
+                else:
+                    temp //= Numbers[opIdx + 1]
         Max = max(Max, temp)
         Min = min(Min, temp)
 
