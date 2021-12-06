fa0941e6691362d6cf25c7e9d199e0ddc0b6bd3a
1998YUKI0331 <dkenl0110@dongguk.edu>
2021-12-06 16:27:52 +0900

    [1998YUKI0331] 동적 계획법 1 11053.py

---
+++ 1998YUKI0331/baekjoon/Dynamic/L_S/11053.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+
+def solution(N, A):
+    dp = [1 for _ in range(N)]  # dp[i]: A[i]가 마지막 원소일 때 LIS
+
+    for i in range(N):
+        for j in range(i + 1):
+            if A[i] > A[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+    print(solution(N, A))
