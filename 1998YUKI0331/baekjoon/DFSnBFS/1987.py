3010d787361201ebbeef4fd19facf67d51520162
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-01-19 16:37:38 +0900

    [1998YUKI0331] 그래프 이론 1987.py

--- 1998YUKI0331/baekjoon/DFSnBFS/1987.py
+++ 1998YUKI0331/baekjoon/DFSnBFS/1987.py
@@ -1,5 +1,9 @@
 """
-틀렸습니다임... 투비 컨티뉴...
+시간초과.
+alphabets.remove(graph[nextX][nextY]) 해주는 이유?
+현재 노드 기준 마지막에 방문한 노드가 최적 루트라는 보장이 없기 때문에
+모든 경우를 순회하기 위해 지워준다.
+안지워주면 A->B->C인 경우랑 C->A->B인 경우 같이 순서 다르게 가는 경우는 순회 안함
 """
 
 import sys
@@ -10,26 +14,25 @@ dX = [-1, 1, 0, 0]
 dY = [0, 0, -1, 1]
 
 
-def solution(x, y, alphabets, cnt):   # DFS
+def solution(x, y, cnt):   # DFS
     global result
-    result = max(result, len(list(set(alphabets))))
-    alphabets.append(graph[x][y])
-    print(x, y, alphabets, cnt)
+    alphabets.add(graph[x][y])
+    result = max(result, cnt)
 
     for i in range(4):
         nextX = x + dX[i]
         nextY = y + dY[i]
         if 0 <= nextX < R and 0 <= nextY < C:
             if graph[nextX][nextY] not in alphabets:
-                solution(nextX, nextY, alphabets, cnt + 1)
+                solution(nextX, nextY, cnt + 1)
+                alphabets.remove(graph[nextX][nextY])
 
 
 if __name__ == '__main__':
     R, C = map(int, input().strip().split())
     graph = [list(map(str, input().strip())) for _ in range(R)]
-    alphabets = []
-    result = 0
-    
-    solution(0, 0, alphabets, 1)
+    alphabets = set()
+    result = 1
 
+    solution(0, 0, result)
     print(result)
